b5c76838bd89f87a501a3d3acac8bf573b72ce4c
zzg <2420467607@qq.com>
2023-10-19 00:25:11 +0800

    install and use mongodb

---
+++ server/db/index.js
@@ -0,0 +1,8 @@
+const mongoose = require('mongoose');
+mongoose.connect('mongodb://127.0.0.1:27017/blog',{useNewUrlParser:true})
+.catch(e=>{
+    console.log('connection error',e.message);
+})
+
+const db=mongoose.connection
+module.exports = db;
--- server/index.js
+++ server/index.js
@@ -2,9 +2,12 @@ const express = require('express');
 const cors = require('cors')
 const app = express();
 const apiPort = 3000;
+const db=require('./db');
 app.use(express.urlencoded({extends:true}));
 app.use(cors());
 app.use(express.json());
+
+db.on('error',console.error.bind(console,'MondoDB connection error'));
 app.get('/',(req,res)=>{
     res.send('Hello World');
 })
